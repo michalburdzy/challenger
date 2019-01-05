22a2350e41a731e63bea3642a5055218666cd120
michal <mmicalt@gmail.com>
2019-01-05 06:10:55 +0100

    added bulma

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -8,21 +8,45 @@ class Navbar extends Component {
   }
 
   render() {
-  console.log(this.props)
 
     if(this.props.user === null || this.props.user === undefined){
       return (
-        <div className="navbar">
-          <a href='/auth/google'>Login with Google</a>
-          <br />
-          <a href='/auth/reddit'>Login with Reddit</a>
+        <div className="navbar is-light" role="navigation" aria-label="main navigation">
+          <div className='navbar-brand'>Challenger</div>
+          <div className='navbar-menu'>
+            <div className='navbar-end'>
+              <div className='navbar-item has-dropdown is-hoverable'>
+                <div className='navbar-link'>
+                  Login
+                </div>
+                <div className='navbar-dropdown'>
+                  <div className='navbar-item'>
+                    <a href='/auth/google'>with Google</a>
+                  </div>
+                  <div className='navbar-item'>
+                    <a href='/auth/reddit'>with Reddit</a>
+                  </div>
+                </div>
+              </div>
+              <div className='navbar-item'></div>
+            </div>
+          </div>
         </div>
       );
     } else {
       return (
-        <div>
-          User: {this.props.user.name}
-          <a href='/api/logout'>Logout</a>
+        <div className="navbar" role="navigation" aria-label="main navigation">
+          <div className='navbar-brand'>Challenger</div>
+          <div className='navbar-menu'>
+            <div className='navbar-end'>
+              <div className='navbar-item'>
+                Hello, {this.props.user.name}!
+              </div>
+              <div className='navbar-item'>
+                <a href='/api/logout'>Logout</a>
+              </div>
+            </div>
+          </div>
         </div>
       )
     }
--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware } from 'redux'
 import reducer from './reducers'
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import 'bulma/css/bulma.css'
 
 
 const store = createStore(reducer, applyMiddleware(thunk))
